2767fc8d758430010e84649c799e9ddd9af08eb5
Max <max.malina13@gmail.com>
2017-04-24 22:34:47 +0300

    Change response

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(80, function(){
     console.log('Server running on 80...');
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello!\n');
+    response.write('856912b5');
     response.end();
 });
