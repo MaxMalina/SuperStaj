60b140164c11714f5e8eb57df5ea1648e579ad1e
Max <max.malina13@gmail.com>
2017-04-24 22:05:50 +0300

    Try to make another response

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(80, function(){
     console.log('Server running on 80...');
-    response.write('856912b5');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello!\n');
+    response.end();
 });
