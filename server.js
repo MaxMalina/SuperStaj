49c6ee262f9db9e4a1a83ddd3d2f69e9dc4a7d5f
Max <max.malina13@gmail.com>
2017-04-24 23:20:40 +0300

    Listen port 8080

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(80, function(){
+connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 80...');
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('856912b5');
