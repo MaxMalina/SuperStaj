8586d468560f1b3173b7603cdba599340695c86c
Max <max.malina13@gmail.com>
2017-04-24 23:36:57 +0300

    Listen port 8888

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 80...');
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('856912b5');
-    response.end();
-});
+//var connect = require('connect');
+//var serveStatic = require('serve-static');
+//connect().use(serveStatic(__dirname)).listen(8080, function(){
+//    console.log('Server running on 80...');
+//    response.writeHead(200, {'Content-Type': 'text/plain'});
+//    response.write('856912b5');
+//    response.end();
+//});
+
+var http = require('http');
+
+function onRequest(request, response)
+{
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.write('856912b5');
+	response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+console.log('OK');
