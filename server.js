975ce65e35d755a10c964a97b55d7138d7ff92f1
Max <max.malina13@gmail.com>
2017-04-29 16:40:14 +0300

    Final

--- server.js
+++ server.js
@@ -24,14 +24,18 @@
 
 var express = require("express");
 var app = express();
-var logger = require('morgan');
-app.use(logger("default"));
+var morgan = require('morgan');
+var logger = morgan('default');
+app.use(logger); //replaces your app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+
+app.on('listening',function() {
   console.log("Listening on " + port);
 });
+
+app.listen(port);
